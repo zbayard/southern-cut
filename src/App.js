ef071ebc2c6401ed9f7dbab230719d18344b5a94
Zach <zbayard@gmail.com>
2021-08-18 13:43:37 -0400

    nav baseline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar.js';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
+    
     </div>
   );
 }
