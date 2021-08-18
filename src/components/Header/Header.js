88e0830b8de3e3d0c0117336aae915c6048ef68e
Zach <zbayard@gmail.com>
2021-08-18 15:30:04 -0400

    header animations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import './Header.css';
 
+
 const Header = ()=>{
 
     return(
         <div className="header">
-            <div class="logo-box">
+            <div className="logo-box">
                 <img src="" alt="Logo" className="logo"/>
             </div>
+            <a href="#" className="btn btn-white btn-animated">Get a quote today</a>
 
-            <a href="#" class="btn btn-white btn-animated">Get a quote today</a>
+            <div className="arrow-box">
+                <img src='arrow' alt="arrow" className="arrow"/>
+            </div>
+
         </div>
     )
 
