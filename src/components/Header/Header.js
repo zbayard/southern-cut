294a11e4748486f482e5ead7a02549c1e1393556
Zach <zbayard@gmail.com>
2021-08-18 15:50:09 -0400

    btn box

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,13 @@ const Header = ()=>{
             <div className="logo-box">
                 <img src="" alt="Logo" className="logo"/>
             </div>
-            <a href="#" className="btn btn-white btn-animated">Get a quote today</a>
+
+            <div className="btn-box">
+                <a href="#" className="btn btn-white btn-animated">Get a quote today</a>
+
+            </div>
+
+            
 
             <div className="arrow-box">
                 <FontAwesomeIcon className="arrow" icon={faAngleDoubleDown}  />
