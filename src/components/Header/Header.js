8136b177736dc20b9cae5879a695da62c048a724
Zach <zbayard@gmail.com>
2021-08-18 14:19:45 -0400

    header started

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+
+const Header = ()=>{
+
+    return(
+        <div className="header">
+            <div class="logo-box">
+                <img src="" alt="Logo" className="logo"/>
+            </div>
+
+            <a href="#" class="btn btn-white btn-animated">Get a quote today</a>
+        </div>
+    )
+
+};
+export default Header;
