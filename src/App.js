473e2e94ebe658b1b44757c55ae32a9542e4d6d3
Zach <zbayard@gmail.com>
2021-08-19 12:35:35 -0400

    started about section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
 import Header from './components/Header/Header.js';
+import AboutUs from './components/About Us/AboutUs.js';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Header/>
-    
+     <AboutUs/>
+
     </div>
   );
 }
---
+++ src/components/About Us/AboutUs.js
@@ -0,0 +1,24 @@
+import './AboutUs.css';
+
+const AboutUs = ()=>{
+    return (
+        <div className="about-us">
+            <div className="w-container">
+                <div className="w-box">
+                    <h1 className="ws">what</h1>
+                </div>
+                <div className="w-box">
+                    <h1 className="ws">when</h1>
+                </div>
+                <div className="w-box">
+                    <h1 className="ws">where</h1>
+                </div>
+                <div className="w-box">
+                <h1 className="ws">why</h1>
+                </div>
+            </div>
+           
+        </div>
+    );
+};
+export default AboutUs;
