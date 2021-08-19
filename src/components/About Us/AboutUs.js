48a2581e573a5ac66636f6e39cfeb5718b9cfdeb
Zach <zbayard@gmail.com>
2021-08-19 15:19:02 -0400

    about transitions

--- src/components/About Us/AboutUs.js
+++ src/components/About Us/AboutUs.js
@@ -13,17 +13,25 @@ const AboutUs = ()=>{
     return (
         <div className="about-us">
             <div className="w-container">
-                <div className="what-box">
+                <div className="w-box what">
                     <h1 className="ws">What</h1>
+                    <h2>What</h2>
+                    <p>some bullshit in here</p>
                 </div>
-                <div className="why-box">
+                <div className="w-box why">
                     <h1 className="ws">Why</h1>
+                    <h2>Why</h2>
+                    <p>some bullshit in here</p>
                 </div>
-                <div className="where-box">
+                <div className="w-box where">
                     <h1 className="ws">Where</h1>
+                    <h2>Where</h2>
+                    <p>some bullshit in here</p>
                 </div>
-                <div className="when-box">
+                <div className="w-box when">
                 <h1 className="ws">When</h1>
+                <h2>When</h2>
+                    <p>some bullshit in here</p>
                 </div>
             </div>
            
