784dd1ac7ecd1acae9cd9286b5c14d2813fb76c8
Zach <zbayard@gmail.com>
2021-08-20 13:22:05 -0400

    services object created, concept in mind

--- src/components/SC Services/Services.js
+++ src/components/SC Services/Services.js
@@ -2,6 +2,30 @@ import './Services.css';
 
 const Services = ()=>{
 
+    const services = [
+        {name: 'Landscape Design', description: ''},
+        {name: 'Planting', description: ''},
+        {name: 'Mulching', description: ''},
+        {name: 'Leaf Control', description: ''},
+        {name: 'Hardscapes', description: ''},
+        {name: 'Irrigation', description: ''},
+        {name: 'Pool Demolition', description: ''},
+        {name: 'Grading', description: ''},
+        {name: 'Hedging', description: ''},
+        {name: 'Concrete', description: ''},
+        {name: 'Drainage', description: ''},
+        {name: 'Rock Installation', description: ''},
+        {name: 'Mowing', description: ''},
+        {name: 'Fertilization', description: ''},
+        {name: 'Aeration', description: ''}
+    ]
+
+    const serviceH2s = services.map((h2)=>{
+       return(
+           <h2>{h2.name}</h2>
+       )
+    });
+
     return(
         <div id="service" className="services">
             <div className="services-container">
@@ -12,7 +36,7 @@ const Services = ()=>{
                     <h1 className="green">all.</h1>
                 </div>
                 <div className='items list'>
-                    <h2>Hello world</h2>
+                    {serviceH2s}
                 </div>
 
 
