b7615ff5c0e37ce52316b3db05905e24b85bf075
Zach <zbayard@gmail.com>
2021-08-20 13:05:38 -0400

    services set up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
 import Header from './components/Header/Header.js';
 import AboutUs from './components/About Us/AboutUs.js';
+import Services from './components/SC Services/Services.js';
 
 const App = ()=>{
   
@@ -10,6 +11,7 @@ const App = ()=>{
       <Navbar/>
       <Header/>
       <AboutUs/>
+      <Services/>
     </div>
   );
 };
---
+++ src/components/SC Services/Services.js
@@ -0,0 +1,24 @@
+import './Services.css';
+
+const Services = ()=>{
+
+    return(
+        <div id="service" className="services">
+            <div className="services-container">
+                <div className='items statement'>
+                    <h1 className="black">We</h1>
+                    <h1 className="black">do</h1>
+                    <h1 className="black">it</h1>
+                    <h1 className="green">all.</h1>
+                </div>
+                <div className='items list'>
+                    <h2>Hello world</h2>
+                </div>
+
+
+            </div>
+        </div>
+    )
+
+};
+export default Services;
