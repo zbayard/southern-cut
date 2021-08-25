90aead3efebebeecf90d69fa7e823908034c8a63
Zach <zbayard@gmail.com>
2021-08-25 10:53:18 -0400

    contact set up

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Navbar from './components/Navbar/Navbar.js';
 import Header from './components/Header/Header.js';
 import AboutUs from './components/About Us/AboutUs.js';
 import Services from './components/SC Services/Services.js';
+import Contact from './components/Contact/Contact.js';
 
 const App = ()=>{
-  
+
   return (
     <div className="App">
       <Navbar/>
       <Header/>
       <AboutUs/>
       <Services/>
+      <Contact/>
     </div>
   );
 };
